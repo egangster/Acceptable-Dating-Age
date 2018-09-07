2cebd7ce3ec4c3f6252255eb2d3a141bec6e98bf
Jeramie <jeramie@protonmail.com>
2018-09-06 20:21:59 -0700

    Update DatingAge.c

--- DatingAge.c
+++ DatingAge.c
@@ -3,14 +3,14 @@
 
 int main() 
 {
-	char cChar[4];
+	char inputChar[4];
 
 	printf("hello please enter your age: ");
-	gets_s(cChar, 3); // compilable on vs2017 for windows, but won't be on POSIX systems, use gets() instead.
+	gets_s(inputChar, 3); // Compilable on VS2017 for Windows, but won't be on POSIX systems, use gets() instead.
 
-	int age = atoi(cChar);
+	int age = atoi(inputChar);
 	int lowestAcceptableAge = age / 2 + 7;
 	int maxAcceptableAge = (age - 7) * 2;
 
-	printf("the socially acceptable age range you can date in is: %i to %i", lowestAcceptableAge, maxAcceptableAge );
+	printf("the socially acceptable age range you can date in is: %i to %i", lowestAcceptableAge, maxAcceptableAge);
 }
