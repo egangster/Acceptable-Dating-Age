58bbc2d269776e31b02d0b37c0720edcea5f69f3
Bryce <28786697+egangster@users.noreply.github.com>
2018-09-07 16:10:13 -0400

    Update DatingAge.kt

--- DatingAge.kt
+++ DatingAge.kt
@@ -8,7 +8,7 @@ fun main( vararg args: String ) {
     val lowestAcceptableAge = localAge / 2 + 7
     val maxAcceptableAge = ( localAge - 7 ) * 2
     if ( localAge > 12 )
-        println( "the socially acceptable age range you can date in is: $lowestAcceptableAge - $maxAcceptableAge" )
+        println( "the socially acceptable age range you can date in is: $lowestAcceptableAge to $maxAcceptableAge" )
     else
         println( "you should not be dating buddy..." )
 }
