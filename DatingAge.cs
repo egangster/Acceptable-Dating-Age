1054c69b89e9c6655f907dc4a3fb465e069ab944
Jeramie <jeramie@protonmail.com>
2018-09-06 19:40:05 -0700

    Add files via upload
    c# version

---
+++ DatingAge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello please enter your age: ");
+            int localAge = Int32.Parse(Console.ReadLine());
+
+            int lowestAcceptableAge = localAge / 2 + 7;
+            int maxAcceptableAge = (localAge - 7) * 2;
+
+            if (localAge > 12)
+            {
+                Console.WriteLine("the socially acceptable age range you can date in is: {0} to {1}", lowestAcceptableAge, maxAcceptableAge);
+            }
+            else
+            {
+                Console.WriteLine("you should not be dating buddy...");
+            }
+        }
+    }
+}
