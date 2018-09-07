de3445a76df5eadcee200c4341b568e94aebdb99
Bryce <28786697+egangster@users.noreply.github.com>
2018-09-06 22:14:56 -0400

    Upload the kotlin version of this project

---
+++ DatingAge.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main( vararg args: String ) {
+    print( "hello please enter your age: " )
+
+    val reader = Scanner( System.`in` )
+    val localAge = reader.nextInt( )
+    val lowestAcceptableAge = localAge / 2 + 7
+    val maxAcceptableAge = ( localAge - 7 ) * 2
+    if ( localAge > 12 )
+        println( "the socially acceptable age range you can date in is: $lowestAcceptableAge - $maxAcceptableAge" )
+    else
+        println( "you should not be dating buddy..." )
+}
